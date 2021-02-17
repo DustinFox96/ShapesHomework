a88aa3519d7522001e87ff2d031b2e6bb768ac9b
Dustin Fox <dustinfox1996@gmail.com>
2021-02-16 21:00:51 -0500

    Add project files.

---
+++ ShapesHomework/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ShapesHomework {
+    class Program {
+        static void Main(string[] args) {
+            var rect1 = new Rect(7, 2);
+
+            Console.WriteLine($"the peremiter of rect1 is {rect1.Peremiter()}");
+
+            var sqr1 = new Sqr(7);
+            Console.WriteLine($"the Area of sqr1 is {sqr1.Area()}");
+        }
+
+    }
+}
---
+++ ShapesHomework/Sqr.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShapesHomework {
+    class Sqr : Rect {
+        public Sqr() : base() { }
+
+        public Sqr(int s1) : base(s1, s1) { }
+    }
+}
